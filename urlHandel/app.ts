77d9aa7e82b5400b026c3f6c794ec2a06b5434b7
miao <miao@xxx.com>
2023-07-18 13:11:08 +0800

    first commit

---
+++ urlHandel/app.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import fetch from 'node-fetch';
+const baseUrl = 'http://127.0.0.1:8080/unsafe/'
+const app = express()
+app.use((req, res, next) => {
+  const { width, height } = req.query
+  res.setHeader('Content-Type', 'image/jpeg')
+  fetch(`${baseUrl}rs:fit:${width || ''}:${height || ''}:1/plain/s3:/${req.path}`, { method: 'get' }).then(data => {
+    data.body.pipe(res)
+  })
+})
+
+app.listen(10000, () => {
+  console.log('listen port in 10000')
+})
